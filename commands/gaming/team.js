6d22f596b839368d924450ca1654066cd69eea0d
Arkaitz <arkaitzvega@hotmail.com>
2025-08-04 23:28:34 +0200

    Add initial team command

---
+++ commands/gaming/team.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, SlashCommandRoleOption, ChatInputCommandInteraction, SlashCommandStringOption, InteractionCallback } = require('discord.js');
+
+const list = [];
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('team')
+        .setDescription('find a teammate')
+        .addRoleOption(new SlashCommandRoleOption()
+            .setName('Game')
+            .setDescription('The game you are looking to play')
+            .setRequired(true)),
+    async execute(/** @type {ChatInputCommandInteraction} */ interaction) {
+        await interaction.reply('Testing ✅' + interaction.options.getRole('Game'));
+        const position = list.indexOf(interaction.user.id)
+        if (position !== -1) {
+            list.splice(position, 1)
+        }
+        if (list.length === 3) {
+            list.shift()
+        }
+        list.push(interaction.user.id)
+    },
+};
