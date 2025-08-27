173a8bc25ac037054d0a0fa2afdea116364e3c81
ArkkBtw <arkaitzvega@hotmail.com>
2025-08-27 18:33:12 +0200

    warning viwed fixed

--- commands/Kibu/working.js
+++ commands/Kibu/working.js
@@ -12,7 +12,7 @@ const { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder,
 // });
 
 const culoembed = new EmbedBuilder()
-    .setColor('#71368A')
+    .setColor('#FEE75C')
     //.set title: prints title
     .setTitle('Ups, este canal está en mantenimiento en este momento')
     //.setURL('https://discord.js.org/')
@@ -25,81 +25,16 @@ const culoembed = new EmbedBuilder()
     .setFooter({ text: 'Powered by Kibu', iconURL: 'https://i.imgur.com/bC5CdRI.png' });
 
 
-
-
-
-// // create new button with style
-// const culobutton = new ButtonBuilder()
-//     // .setCustomId('button1')
-//     .setLabel("button label")
-//     .setURL("https://lol.es")
-//     .setStyle(ButtonStyle.Link)
-
-// // create new row  (Action Row Builder is needed to print the buttons)    
-// const culorow = new ActionRowBuilder().addComponents(culobutton)
-
-
 module.exports = {
 
 
     data: new SlashCommandBuilder()
-        .setName('ayuda')
-        .setDescription('muestra todos los comandos'),
+        .setName('warning')
+        .setDescription('genera el mensaje de mantenimiento'),
     async execute(interaction) {
 
-        await interaction.reply('Kibu esta buscando...');
+        await interaction.reply('Kibu esta pensando...');
         await interaction.channel.send({ embeds: [culoembed] })
-        //await interaction.channel.send({ components: [culorow] })
-
+    
     },
 };
-// data: new SlashCommandBuilder()
-//     .setName('shop')
-//     .setDescription('obten el link directo a la tienda de BWF')
-//     .addRoleOption(new SlashCommandRoleOption()
-//         .setName('juego')
-//         .setDescription('Elige un rol que describa el juego que buscas')
-//         .setRequired(true)),
-module.exports = {
-
-
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('muestra todos los comandos'),
-    async execute(interaction) {
-
-        await interaction.reply('Kibu esta buscando...');
-        await interaction.channel.send({ embeds: [culoembed] })
-        //await interaction.channel.send({ components: [culorow] })
-
-    },
-};
-// async execute(/** @type {ChatInputCommandInteraction<'cached'>} */ interaction) {
-
-//const game = interaction.options.getRole('juego')
-
-// if (!gamesOfRecentPeople.has(game.id)) {
-//     gamesOfRecentPeople.set(game.id, [])
-
-// }
-
-// const list = gamesOfRecentPeople.get(game.id)
-
-// const position = list.indexOf(interaction.user.id)
-
-// if (position !== -1) {
-//     list.splice(position, 1)
-// }
-// if (list.length === 3) {
-//     list.shift()
-// }
-// list.push(interaction.user.id)
-
-// console.log("the user", list, "selected the role", game.id)
-// console.log(list)
-
-// await interaction.channel.send(`${userMention(list[0])}, ${userMention(list[1])}, ${userMention(list[2])}>`)
-//await interaction.channel.send
-// const message = await interaction.channel.send({ embeds: [createculoembed([])] })
-// await message.edit({ embeds: [createculoembed(list)] })
-// //await interaction.channel.send({ components: [culorow] })
