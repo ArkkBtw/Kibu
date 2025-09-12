b13257da4e37659f208a7decff9390cd5a2628e8
ArkkBtw <arkaitzvega@hotmail.com>
2025-09-12 15:53:55 +0200

    removed test version added kibu version config

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.listen(port, () => {
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
-const { token } = require('./configtest.json');
+const { token } = require('./config.json');
 
 
 // Create a new client instance
